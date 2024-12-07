68b117c0a81b7c22d91899e21fc3b677fc7ce1e9
Nguyen Huu Dang <nguyenhuudan4811@gmail.com>
2024-12-07 12:00:41 +0700

    Add project files.

---
+++ WebApplication18/Repositories/ICategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using WebApplication18.Models;
+
+namespace WebApplication18.Repositories
+{
+    public interface ICategoryRepository
+    {
+        Task<IEnumerable<Category>> GetAllAsync();
+        Task<Category> GetByIdAsync(int id);
+        Task AddAsync(Category category);
+        Task UpdateAsync(Category category);
+        Task DeleteAsync(int id);
+    }
+}
